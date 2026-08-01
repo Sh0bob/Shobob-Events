62abb791ad62f4b7f1636f8e02424daacbac1b9b
Sh0bob <shobobg@gmail.com>
2026-08-02 00:35:28 +1000

    Update patches.js

--- patch-tools/patches.js
+++ patch-tools/patches.js
@@ -91,9 +91,9 @@ source = replaceOne(
 			top = aQZ.fD + .30 * aQZ.j,
 			inputW = panelW - colorSize - controlGap,
 			buttonTop = top + inputH + controlGap,
-			panelPadX = Math.max(18, .075 * aQZ.i),
-            panelPadTop = Math.max(18, .085 * aQZ.j),
-            panelPadBottom = Math.max(16, .075 * aQZ.j),
+			panelPadX = Math.max(18, .100 * aQZ.i),
+            panelPadTop = Math.max(18, .115 * aQZ.j),
+            panelPadBottom = Math.max(16, .100 * aQZ.j),
 			panelH = (buttonTop + (buttonH + buttonGap) * 2 + buttonH + panelPadBottom) - (top - panelPadTop);
 
 		sd = top;
